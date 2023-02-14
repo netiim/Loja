0ecd72819b69a13f57154e18714d31df8ebbf027
Paulo Neto <paulofrancisco97@gmail.com>
2023-02-14 20:00:43 -0300

    Utilizando o AddRange
    utilizando uma outra forma para salvar os dados no banco o addRange que permite inserir mais de um item na lista de uma vez.

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -17,13 +17,18 @@ namespace Alura.Loja.Testes.ConsoleApp
         private static void GravarUsandoEntity()
         {
             Produto p = new Produto();
-            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Nome = "Jogos Vorazes em Chamas";
             p.Categoria = "Livros";
-            p.Preco = 19.89;
+            p.Preco = 20.89;
+
+            Produto p2 = new Produto();
+            p2.Nome = "Matrix";
+            p2.Categoria = "Livros";
+            p2.Preco = 36.50;
 
             using (var contexto = new LojaContext())
             {
-                contexto.Produtos.Add(p);
+                contexto.Produtos.AddRange(p, p2);
                 contexto.SaveChanges();
             }
         }
